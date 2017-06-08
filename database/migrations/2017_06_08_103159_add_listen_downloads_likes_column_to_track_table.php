99f1be84c00ea28e410b5b784d36823eb810554c
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-08 13:54:37 +0200

    Add Stats Columns

---
+++ database/migrations/2017_06_08_103159_add_listen_downloads_likes_column_to_track_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddListenDownloadsLikesColumnToTrackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            $table->integer('downloads')
+                   ->default(0)
+                   ->after('genre');
+            $table->integer('played')
+                   ->default(0)
+                   ->after('genre');
+            $table->integer('likes')
+                   ->default(0)
+                   ->after('genre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            
+            $table->dropColumn('downloads');
+            $table->dropColumn('played');
+            $table->dropColumn('likes');
+        });
+    }
+}
