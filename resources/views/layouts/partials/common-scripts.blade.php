d90a33e72d172ebd19aec91f2587a79740564e95
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-19 17:22:03 +0200

    Fix album cover errror

--- resources/views/layouts/partials/common-scripts.blade.php
+++ resources/views/layouts/partials/common-scripts.blade.php
@@ -10,16 +10,12 @@
 	<script src="{{url('theme/js/core/source/AppNavSearch.js')}}"></script>
 	<script src="{{url('theme/js/core/source/AppVendor.js')}}"></script>
 
-	<script src='{{url('js/smooth-div-scroll/js/jquery-ui-1.10.3.custom.min.js')}}' type="text/javascript"></script>
-    <script src='{{url('js/smooth-div-scroll/js/jquery.mousewheel.min.js')}}' type="text/javascript"></script>
-    <script src='{{url('js/smooth-div-scroll/js/jquery.kinetic.min.js')}}' type="text/javascript"></script>
-    <script src='{{url('js/smooth-div-scroll/js/jquery.smoothdivscroll-1.3-min.js')}}' type="text/javascript"></script>
 	
+	<script src="http://connect.soundcloud.com/sdk.js"></script>	
 	<script src="{{url('player/js/jquery.gearplayer.libs.min.js')}}"></script>
 	<script src="{{url('player/js/jquery.gearplayer.js')}}"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/plugins/ScrollToPlugin.min.js"></script>
-	<script src="/player/js/app.js"></script>
-	<script src="http://connect.soundcloud.com/sdk.js"></script>
+	<script src="{{url('player/js/app.js')}}"></script>
 
 	<script src="{{mix('js/app.js')}}"></script>
 
