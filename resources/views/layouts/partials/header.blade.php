f37b4b7d6c9785645b82cde662f841d4c4fc0dbc
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-20 09:05:12 +0200

    Swap Menu Toggle button

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,6 +3,11 @@
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="headerbar-left">
 					<ul class="header-nav header-nav-options">
+					<li>
+							<a class="btn btn-icon-toggle menubar-toggle" data-toggle="menubar" href="javascript:void(0);">
+								<i class="fa fa-bars"></i>
+							</a>
+						</li>
 						<li class="header-nav-brand" >
 							<div class="brand-holder">
 								<a href="{{route('home')}}">
@@ -10,11 +15,7 @@
 								</a>
 							</div>
 						</li>
-						<li>
-							<a class="btn btn-icon-toggle menubar-toggle" data-toggle="menubar" href="javascript:void(0);">
-								<i class="fa fa-bars"></i>
-							</a>
-						</li>
+						
 					</ul>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
