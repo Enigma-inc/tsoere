ceedd207c8af6f84a70a4f01bec595fac51eb965
RammelekeLebese <seramerammeleke@gmail.com>
2017-06-21 12:19:44 +0200

    Feature: add player to single track page

--- resources/views/artist/partials/single-track-body.blade.php
+++ resources/views/artist/partials/single-track-body.blade.php
@@ -1,26 +1,38 @@
-<section>
+<div>
     <div class="section-header margin-top-20">
     </div>
-    <div class="section-body no-margin" id ="top">
+    <div class="section-body no-margin">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-sm-offset-3  col-md-6 col-md-offset-3">
-                @if($track)            
+            @if($track)
+            <player player-container="{{'#player-'.$track->id}}" audio="{{$track->audio}}"  inline-template>            
                 <div class="card card-bordered style-default-dark single-track-card">
                     <div class="card-body">
                         <div class="artwork" style="background-image: url('{{url($track->artwork)}}');">
-                            <div id="play-button-{{$track->id}}" class="play-button " data-gearPath="{{url($track->json)}}" >
-                                <a href="#top"><i class="fa fa-play-circle-o"></i></a>
+                            <div class="play-button" @click="playAudio()">
+                                <a href="#"><i :class="[playerActionClass]"></i></a>
                             </div>
                         </div>
                         <div class="details">
                             <div class="header">
-                                <div class="artist-name text-primary text-bold">{{$profile->name}}</div>
-                                <div class="track-title">{{$track->title}}</div>
+                                    <div class="artist-name text-primary text-bold">{{$track->artist->name}}</div>                            
+                                    <div class="track-title">{{$track->title}}</div>
+                            </div>
+                             <div class="player-container">
+                                       <div  id="player-{{$track->id}}"></div>
+                                       <div v-if="loading" class="loading">
+                                              <span class="text-primary" v-cloak>@{{loadingText}}</span>
+                                       </div>
+                            </div>
+                             <div class="info-container">
+                                   <span v-if="elapsedTime" v-cloak>@{{elapsedTime|time}}</span>
+                                   <span v-if="audioDuration" v-cloak>@{{audioDuration|time}}</span>
                             </div>
                             <track-actions :track="{{$track}}"></track-actions>
                         </div>
                     </div>
                 </div>
+                </player>
                 @else
                 <div><h3>Track Not Found</h3></div>
                 @endif
@@ -29,35 +41,19 @@
 
         </div>
     </div>
-</section>
-<section>
+</div>
+<div>
     <div class="page-header margin-10 padding-bottom-10">
     <ol class="breadcrumb"><li class="active">Other Tracks By {{ $profile->name}}</li></ol>
     </div>
-    <div class="section-body no-margin" id="top">
+    <div class="section-body no-margin">
 
         <div class="row">
               @foreach($relatedTracks as $track)
                 <div class="col-xs-12 col-sm-6 col-md-4">                  
-                   <div class="card card-bordered style-default-dark track-card">
-                        <div class="card-body">
-                            <div class="artwork" style="background-image: url('{{url($track->artwork)}}');">
-                                <div id="play-button-{{$track->id}}" class="play-button " data-gearPath="{{url($track->json)}}" >
-                                        <a href="#top"><i class="fa fa-play-circle-o"></i></a>
-                                    </div>
-                            </div>
-                            <div class="details">
-                                <div class="header">
-                                    <div class="artist-name text-primary text-bold">{{$profile->name}}</div>                            
-                                    <div class="track-title">{{$track->title}}</div>
-                                </div>
-                                 <track-actions :track="{{$track}}"></track-actions>
-                                  </div>
-                                </div>
-                            </div>
-                        </div>
-                    
+                     @include('player.multi-track-player') 
+                </div>    
                 @endforeach
         </div>
     </div>
-</section>
+</div>
