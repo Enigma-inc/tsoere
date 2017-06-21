fe415cab44589eb3bec42fa36ae5c1798a874b1e
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-21 11:09:47 +0200

    Remove Gear Player

--- resources/views/layouts/partials/common-scripts.blade.php
+++ resources/views/layouts/partials/common-scripts.blade.php
@@ -9,15 +9,5 @@
 	<script src="{{url('theme/js/core/source/AppCard.js')}}"></script>
 	<script src="{{url('theme/js/core/source/AppNavSearch.js')}}"></script>
 	<script src="{{url('theme/js/core/source/AppVendor.js')}}"></script>
-	<script src="{{url('js/wavesurfer.min.js')}}"></script>
-
-	
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  
-<script src="http://connect.soundcloud.com/sdk.js"></script>	
-	<script src="{{url('player/js/jquery.gearplayer.libs.min.js')}}"></script>
-	<script src="{{url('player/js/jquery.gearplayer.js')}}"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/plugins/ScrollToPlugin.min.js"></script>
-
 	<script src="{{mix('js/app.js')}}"></script>
 
