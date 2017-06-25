8438981cbdda5bbc9811f701ae96158c3366effa
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-25 13:44:04 +0200

    Add

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -34,7 +34,27 @@
                         infinite: true,
                         autoplay: true,
                         autoplaySpeed: 5000,
-                        slidesToShow:6
+                        slidesToShow:6,
+                        responsive:[
+                            {
+                                breakpoint:1024,
+                                settings:{
+                                    slidesToShow:5,
+                                }
+                            },
+                            {
+                                breakpoint:600,
+                                settings:{
+                                    slidesToShow:3,
+                                }
+                            },
+                            {
+                                breakpoint:480,
+                                settings:{
+                                    slidesToShow:2,
+                                }
+                            }
+                        ]
                     }
                 );
             });
