7f6e74431a365f7e3c186f9ac7b78bd2c4785eeb
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-29 11:36:02 +0200

    Footer Slider Break points

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -75,23 +75,27 @@
                         autoplay: true,
                         autoplaySpeed: 5000,
                         slidesToShow:6,
+                         slidesToScroll:6,
                         responsive:[
                             {
                                 breakpoint:1024,
                                 settings:{
-                                    slidesToShow:5,
+                                    slidesToShow:3,
+                                     slidesToScroll:3,
                                 }
                             },
                             {
                                 breakpoint:600,
                                 settings:{
                                     slidesToShow:3,
+                                     slidesToScroll:3,
                                 }
                             },
                             {
                                 breakpoint:480,
                                 settings:{
                                     slidesToShow:2,
+                                     slidesToScroll:2,
                                 }
                             }
                         ]
