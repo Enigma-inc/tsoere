cbb7d1d6d1d9f73c3a623c963fe1c38b0aac201e
Neo Mokoena <mokoena.n.a@gmail.com>
2017-06-29 11:33:12 +0200

    Test Device Breakpoints

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -47,7 +47,8 @@
                             {
                                 breakpoint:1024,
                                 settings:{
-                                    slidesToShow:3,
+                                    slidesToShow:2,
+                                    slidesToShow:2,
                                 }
                             },
                             {
