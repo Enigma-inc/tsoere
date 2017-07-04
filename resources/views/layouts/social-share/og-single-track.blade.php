0195e2ef96c1ecd3221c622be1f6fac917d0c1ef
Neo Mokoena <mokoena.n.a@gmail.com>
2017-07-04 10:34:12 +0200

    Deploy Track Pages Open Graph

--- resources/views/layouts/social-share/og-single-track.blade.php
+++ resources/views/layouts/social-share/og-single-track.blade.php
@@ -1,8 +1,9 @@
-<meta property="og:title" content="{{$track->title}}" />
-<meta property="og:type" content="music.song" />
-<meta property="og:url" content="http://www.musicbox.co.ls" /> <!--The canonical URL of your song.-->
-<meta property="og:image" content="{{$track->artwork}}"/>	<!-- An image URL which should represent your song within the graph (likely the album art). The image must be at least 50px by 50px (though minimum 200px by 200px is preferred) and have a maximum aspect ratio of 3:1. We support PNG, JPEG and GIF formats-->
-<meta property="og:music:musician" content="{{$profile->name}}"/>
-<meta property="og:site_name" content="Musicbox"/> <!--a human readable name of your site-->
-<meta property="og:audio" content="{{$track->path}}"/>  <!--url to play this song-->
-<meta property="og:audio:type" content="audio/vnd.facebook.bridge"/>
+<meta property="og:image" content="{{$track->artwork}}">
+<meta property="og:image:width" content="279">
+<meta property="og:image:height" content="279">
+<meta property="og:description" content="LISTEN - DOWNLOAD - SHARE ON MUSICBOX">
+<meta property="og:title" content="{{$profile->name}} | {{$track->title}}">
+<meta property="og:url" content="http://musicbox.co.ls">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Musicbox"/>
+
