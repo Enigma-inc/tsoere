ad0455c5b98b72cc40d8da7fe2d35e1a116d531e
Mochesane <mpalilabmannkheekhe@gmail.com>
2017-07-07 17:54:46 +0100

    Minify scripts with gulp

--- common-scripts.blade.php
+++ common-scripts.blade.php
@@ -1,4 +1,4 @@
-  <script src="{{url('theme/js/bundle/scripts.min.js')}}"></script>
+  <script src="{{url('theme/js/bundle/scripts.min.js?rev=@@hash')}}"></script>
 
   <script type="text/javascript" src="{{mix('js/wall.js')}}"></script>
 
